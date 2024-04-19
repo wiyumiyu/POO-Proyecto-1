5e8304156a53e7bb70de3a00317b60e053c19e69
Mariana Torres <mariana.torres.valverde0@gmail.com>
2024-04-19 09:06:48 -0600

    inicio prueba

---
+++ src/main/java/wiyu/barberia_poo/Barberia_POO.java
@@ -0,0 +1,12 @@
+package wiyu.barberia_poo;
+
+/**
+ *
+ * @author maria
+ */
+public class Barberia_POO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
