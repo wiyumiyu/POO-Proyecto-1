0a93dda34331475ff15f17ef64e2b0f57bdd6d96
Mariana Torres <mariana.torres.valverde0@gmail.com>
2024-04-24 14:55:19 -0600

    Creacion horario2

--- src/main/java/wiyu/barberia_poo/Clases/HorarioBarberia.java
+++ src/main/java/wiyu/barberia_poo/Clases/HorarioBarberia.java
@@ -2,6 +2,8 @@ package wiyu.barberia_poo.Clases;
 
 import java.time.Duration;
 import java.time.LocalTime;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  *
@@ -9,24 +11,38 @@ import java.time.LocalTime;
  */
 public class HorarioBarberia {
     
-    public static long calcularHoras(LocalTime horaInicio, LocalTime horaSalida) {
-        // Calcular la diferencia de tiempo
-        Duration duracion = Duration.between(horaInicio, horaSalida);
-        
-        // Verificar si los minutos son diferentes de cero
-        if (tieneMinutos(duracion)) {
-            throw new IllegalArgumentException("Error: La diferencia de tiempo no puede tener minutos.");
-        }
-        
-        // Obtener las horas de la duración
-        long horas = duracion.toHours();
-        
-        return horas;
-    }
+    // Ejemplo para usar la funcion de obtener los procedimientos
     
-    // Función para verificar si la duración tiene minutos
+//    LocalTime horaInicio = LocalTime.of(8, 30); // Por ejemplo, 8:00 AM
+//    LocalTime horaFin = LocalTime.of(18, 30); // Por ejemplo, 6:00 PM
+//    generarMapaHorasConCodigo(horaInicio, horaFin); 
+    
+    public static void generarMapaHorasConCodigo(LocalTime inicio, LocalTime fin) {
+        Duration duracion = Duration.between(inicio, fin);
+        if (tieneMinutos(duracion)) {
+            throw new IllegalArgumentException("Horario inválido, el rango de horarios tiene que tener horas exactas para poder brindar su servicio");
+        }
+
+        Map<Integer, LocalTime> horasConCodigo = new HashMap<>();
+        LocalTime horaActual = inicio;
+        int codigo = 1; // Inicializar el código
+
+        while (horaActual.isBefore(fin)) {
+            horasConCodigo.put(codigo, horaActual);
+            horaActual = horaActual.plusHours(1);
+            codigo++; // Incrementar el código para la siguiente hora
+        }
+
+//        // Imprimir el mapa (solo para verificar)
+//        for (Map.Entry<Integer, LocalTime> entry : horasConCodigo.entrySet()) {
+//            int codigoHora = entry.getKey();
+//            LocalTime hora = entry.getValue();
+//            System.out.println("Código: " + codigoHora + ", Hora: " + hora);
+//        }
+    }
+
     public static boolean tieneMinutos(Duration duracion) {
-        return duracion.toMinutes() % 60 != 0;
+        return duracion.getSeconds() % 3600 != 0; // Verificar si hay minutos en la duración
     }
     
 }
