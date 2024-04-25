9b9d928332bd5f09576c33b843b060e369ab7f0d
Mariana Torres <mariana.torres.valverde0@gmail.com>
2024-04-25 08:56:00 -0600

    Horario finalizado

--- src/main/java/wiyu/barberia_poo/Clases/HorarioBarberia.java
+++ src/main/java/wiyu/barberia_poo/Clases/HorarioBarberia.java
@@ -11,17 +11,30 @@ import java.util.Map;
  */
 public class HorarioBarberia {
     
+    private String dia;
+    private LocalTime inicio;
+    private LocalTime fin;
+    private Map<Integer, LocalTime> horasConCodigo;
+    
+    public HorarioBarberia(String dia, LocalTime inicio, LocalTime fin) {
+        this.dia = dia;
+        Duration duracion = Duration.between(inicio, fin);
+        if (!tieneMinutos(duracion)) {
+            this.inicio = inicio;
+            this.fin = fin;
+        }else{
+            throw new IllegalArgumentException("Horario inválido, el rango de horarios tiene que tener horas exactas para poder brindar su servicio");
+        }
+        this.horasConCodigo = generarMapaHorasConCodigo(inicio, fin);          
+    }
+    
     // Ejemplo para usar la funcion de obtener los procedimientos
     
 //    LocalTime horaInicio = LocalTime.of(8, 30); // Por ejemplo, 8:00 AM
 //    LocalTime horaFin = LocalTime.of(18, 30); // Por ejemplo, 6:00 PM
 //    generarMapaHorasConCodigo(horaInicio, horaFin); 
     
-    public static void generarMapaHorasConCodigo(LocalTime inicio, LocalTime fin) {
-        Duration duracion = Duration.between(inicio, fin);
-        if (tieneMinutos(duracion)) {
-            throw new IllegalArgumentException("Horario inválido, el rango de horarios tiene que tener horas exactas para poder brindar su servicio");
-        }
+    public static Map<Integer, LocalTime> generarMapaHorasConCodigo(LocalTime inicio, LocalTime fin) {
 
         Map<Integer, LocalTime> horasConCodigo = new HashMap<>();
         LocalTime horaActual = inicio;
@@ -32,6 +45,8 @@ public class HorarioBarberia {
             horaActual = horaActual.plusHours(1);
             codigo++; // Incrementar el código para la siguiente hora
         }
+        
+        return horasConCodigo;
 
 //        // Imprimir el mapa (solo para verificar)
 //        for (Map.Entry<Integer, LocalTime> entry : horasConCodigo.entrySet()) {
@@ -43,6 +58,17 @@ public class HorarioBarberia {
 
     public static boolean tieneMinutos(Duration duracion) {
         return duracion.getSeconds() % 3600 != 0; // Verificar si hay minutos en la duración
+    }
+    
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Horario para ").append(dia).append(": de ").append(inicio).append(" a ").append(fin).append("\n");
+        sb.append("Horarios del día:\n");
+        for (Map.Entry<Integer, LocalTime> entry : horasConCodigo.entrySet()) {
+            sb.append("Código: ").append(entry.getKey()).append(", Hora: ").append(entry.getValue()).append("\n");
+        }
+        return sb.toString();
     }
     
 }
