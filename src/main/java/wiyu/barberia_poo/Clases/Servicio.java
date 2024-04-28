3e031de0c338be647b2e8958e0a7b4d75ef7acc2
justbetheleaf <mandi.ramirez14@gmail.com>
2024-04-28 00:17:31 -0600

    cambios servicio

--- src/main/java/wiyu/barberia_poo/Clases/Servicio.java
+++ src/main/java/wiyu/barberia_poo/Clases/Servicio.java
@@ -9,10 +9,53 @@ Mariana Torres Valverde
 
 public class Servicio {
     
+    public static int consecutivo = 1;
+    private int codigo;
+    private String tipo;
+    private String descripcion;
     
+    public Servicio (String tipo, String descripcion){
+        this.tipo = tipo;
+        this.descripcion = descripcion;
+        this.codigo = consecutivo;
+        consecutivo++;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public void modificarServicio(String tipo, String descripcion){
+        this.tipo = tipo;
+        this.descripcion = descripcion;
+    }
     
-    
-    
-    
+    @Override
+    public String toString() {
+        String result =  "Código del servicio: " + this.codigo +"\n";
+        result +=  "Tipo: " + this.tipo +"\n";
+        result += "Descripción: " + this.descripcion +"\n";
+        return result;
+    }
     
 }
